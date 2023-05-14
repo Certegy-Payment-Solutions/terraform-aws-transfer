dbd90fe1000553eb6c40146e0351ccd0e7de73df
ashokpokharel977 <ashokpokharel977@gmail.com>
2023-05-14 10:05:40 +0545

    changed output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "r53_record_fqdn" {
-  value = element(concat(aws_route53_record.this.*.fqdn, [""]), 0)
-}
-
 output "transfer_server_endpoint" {
   value = element(concat(aws_transfer_server.this.*.endpoint, [""]), 0)
 }
